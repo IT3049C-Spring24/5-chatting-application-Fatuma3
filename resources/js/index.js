a2735b554475e670ad9235451c2f378b4f20f98d
FA33 <144830259+Fatuma3@users.noreply.github.com>
2024-02-01 15:04:16 -0500

    Skeleton of the javascript file

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,20 +5,24 @@ const chatBox = document.getElementById('chat');
 
 sendButton.addEventListener('click', () => {
   
-    const id = messageInput.length
+    //const id = messageInput.dataset.id;
     const sender = nameInput.value;
     const text = messageInput.value;
-    const timestamp = new Date().toLocaleTimeString();
+    const timestamp = new Date(message.time);
+    const time = message.getHours() + ":" + message.getMinutes() + ":" + message.getSeconds();  
     
-    console.log(message);
+    const message = {id, text, sender, timestamp};
 });
 
-fetchMessage(){
-  const message = {id, text, sender, timestamp};
+function fetchMessage(){
+
 };
 
-updateMessageInChatBox = (message) => {
+function updateMessageInChatBox(){
 
 }
 
-sendMessages = (message) => []
+sendMessages.addEventListener(click,sendButton){
+  
+
+}
